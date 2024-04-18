098a9ff990252708c29c555c613568ed58b20e9d
Shreya Gurram <gurram13775@gmail.com>
2024-04-18 13:50:16 +0530

    fixes server time issues

--- frontendi/app/components/ui/car-hover-effect.tsx
+++ frontendi/app/components/ui/car-hover-effect.tsx
@@ -23,7 +23,13 @@ export const HoverEffect = ({
   // Function to format datetime to human-readable format
   const formatDateTime = (dateTimeString: string) => {
     const date = new Date(dateTimeString);
-    return date.toLocaleString("en-US");
+    const year = date.getUTCFullYear();
+    const month = ("0" + (date.getUTCMonth() + 1)).slice(-2);
+    const day = ("0" + date.getUTCDate()).slice(-2);
+    const hours = ("0" + date.getUTCHours()).slice(-2);
+    const minutes = ("0" + date.getUTCMinutes()).slice(-2);
+    const seconds = ("0" + date.getUTCSeconds()).slice(-2);
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} UTC`;
   };
 
   //items = items.filter((item) => item.Completed == false);
